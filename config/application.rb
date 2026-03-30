014378848e00c515af59351a22c945de3f5e09cb
Biki Maharjan <beekimrj@gmail.com>
2026-03-30 22:49:03 +0800

    BM: Add APP_TIMEZONE env value for configuring timezone

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,10 @@ module ChurchApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = ENV.fetch("APP_TIMEZONE", "UTC")
+    # This ensures Active Record stores times in the DB as UTC (best practice)
+    # but converts them to your APP_TIMEZONE when you read them.
+    config.active_record.default_timezone = :utc
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
